1702285922acbf313a540cee765b80465d8f2c39
AdamDono <flipbrickzmusic1@gmail.com>
2025-02-28 02:25:49 +0200

    added db

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import psycopg2
+from werkzeug.security import generate_password_hash, check_password_hash
 
 def get_db_connection():
     conn = psycopg2.connect(
@@ -9,3 +10,25 @@ def get_db_connection():
         port="5433"
     )
     return conn
+
+
+def create_user(username, email, password):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    password_hash = generate_password_hash(password)
+    cur.execute(
+        'INSERT INTO users (username, email, password_hash) VALUES (%s, %s, %s);',
+        (username, email, password_hash)
+    )
+    conn.commit()
+    cur.close()
+    conn.close()
+
+def get_user_by_email(email):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM users WHERE email = %s;', (email,))
+    user = cur.fetchone()
+    cur.close()
+    conn.close()
+    return user
